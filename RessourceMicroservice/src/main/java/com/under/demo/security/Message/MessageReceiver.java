350da533246f5c67eab53fa2bb212a481870f8e2
tcatonet <catonethomas@yahoo.fr>
2021-06-14 21:33:54 +0200

    rabbitmq

---
+++ RessourceMicroservice/src/main/java/com/under/demo/security/Message/MessageReceiver.java
@@ -0,0 +1,14 @@
+package com.under.demo.security.Message;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+
+@RabbitListener(queues = "hello")
+public class MessageReceiver {
+
+    @RabbitHandler
+    public void receive(String in) {
+        System.out.println(" [x] Received '" + in + "'");
+    }
+}
